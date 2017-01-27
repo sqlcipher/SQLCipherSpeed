a1a86f85790cb4087f31d49706ff4aa4e9adcf7e
Stephen Lombardo <sjlombardo@zetetic.net>
2017-01-27 13:27:23 -0500

    updated to work with sqlcipher static and app submission

---
+++ sqlcipher-static-ios/demos/sqlcipher-ios-demo/sqlcipher-ios-demo/AppDelegate.h
@@ -0,0 +1,16 @@
+//
+//  AppDelegate.h
+//  sqlcipher-ios-demo
+//
+//  Copyright © 2016 Zetetic. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ sqlcipher-static-ios/demos/sqlcipher-ios-demo/sqlcipher-ios-demo/ViewController.h
@@ -0,0 +1,16 @@
+//
+//  ViewController.h
+//  sqlcipher-ios-demo
+//
+//  Copyright © 2016 Zetetic. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#include "sqlite3.h"
+
+@interface ViewController : UIViewController
+
+@property (weak, nonatomic) IBOutlet UITextView *textOutput;
+
+@end
+
